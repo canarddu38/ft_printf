0d365e7a63a152ae2e6b2792fb7bfcc228a094a3
canarddu38 <71982379+canarddu38@users.noreply.github.com>
2025-11-11 00:29:40 +0100

    added hex and decimal conversions

--- ft_printf.c
+++ ft_printf.c
@@ -6,19 +6,30 @@
 /*   By: kali <kali@student.42lyon.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/10 21:51:22 by kali              #+#    #+#             */
-/*   Updated: 2025/11/10 23:46:25 by kali             ###   ########lyon.fr   */
+/*   Updated: 2025/11/11 00:25:45 by kali             ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./libft/libft.h"
 #include <stdarg.h> 
 
+void	display_hex(size_t nb, int in_lowercase)
+{
+	char	output;
+
+	if (nb == 0)
+		return ;
+	display_hex(nb / 16, in_lowercase);
+	output = nb % 16;
+	if (output >= 10)
+		output += ('A' + 32 * (in_lowercase != 0)) - 10;
+	else
+		output += '0';
+	ft_putchar_fd(output, 1);
+}
+
 static int	print_format(char *type, va_list args)
 {
-	char	*output;
-	size_t	output_len;
-	
-	output_len = 0;
 	if (!type || !type[1])
 		return (-1);
 	if (type[1] == '%')
@@ -26,18 +37,17 @@ static int	print_format(char *type, va_list args)
 	else if (type[1] == 'c')
 		ft_putchar_fd((char)va_arg(args, int), 1);
 	else if (type[1] == 's')
+		ft_putstr_fd(va_arg(args, char *), 1);
+	else if (type[1] == 'p')
 	{
-		output = va_arg(args, char *);
-		output_len = ft_strlen(output);
-		ft_putstr_fd(output, 1);
-		return (output_len);
+		size_t nb = (size_t)va_arg(args, size_t);
+		if (nb < 0)
+			ft_putchar_fd('-', 1);
+		ft_putstr_fd("0x", 1);
+		display_hex(nb, 1);
 	}
 	else if (type[1] == 'd' || type[1] == 'i')
-	{
-		output_len = (size_t)va_arg(args, int);
-		ft_putnbr_fd(output_len, 1);
-		while ()
-	}
+		ft_putnbr_fd((size_t)va_arg(args, int), 1);
 	return (1);
 }
 
@@ -52,19 +62,16 @@ int	ft_printf(const char *format, ...)
 	va_start(args, format);
 	while (((char *)format)[i])
 	{
+		//printf("=>%c\n", ((char *)format)[i]);
 		if (((char *)format)[i] == '%')
 		{
-			j = print_format(&((char *)format)[i], &args);
-			if (j >= 0)
-				i += j + 1;
-			else
+			if (print_format(&((char *)format)[i], &args) < 0)
 				return (-1);
-		}
-		else
-		{
-			ft_putchar_fd(((char *)format)[i], 1);
 			i++;
 		}
+		else
+			ft_putchar_fd(((char *)format)[i], 1);
+		i++;
 	}
 	va_end(args);
 	return (i);
@@ -72,5 +79,7 @@ int	ft_printf(const char *format, ...)
 
 void	main(void)
 {
-	ft_printf("Hello World! I'm %cules and I'm %s\n[]", 'j', "18 years old!");
+	ft_printf("Hello World! I'm %culcleme and I'm %s\nAnd now I'm a student at %i for the next %d years!\n", 'j', "18 years old!", 42, 5);
+	char	*str = "This str is located at address %p\n";
+	ft_printf(str, str);
 }
